0ec602e5fd40045b567c738177d2003008d12c74
澤田皇汰 <sawadakota@ksawada.local>
2016-01-30 15:09:02 +0900

    ・日記のモデルを作った

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160123003737) do
+ActiveRecord::Schema.define(version: 20160128075957) do
+
+  create_table "memories", force: :cascade do |t|
+    t.string   "text",           limit: 255
+    t.text     "image",          limit: 65535
+    t.integer  "user_friend",    limit: 4
+    t.string   "no_user_friend", limit: 255
+    t.datetime "created_at",                   null: false
+    t.datetime "updated_at",                   null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  limit: 255, default: "", null: false
