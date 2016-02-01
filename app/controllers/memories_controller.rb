3a7017eccfceaf65cc191d0fa0cef2b0438dbc3a
澤田皇汰 <sawadakota@ksawada.local>
2016-02-01 18:01:23 +0900

    ・memoryを投稿できるようにした

--- app/controllers/memories_controller.rb
+++ app/controllers/memories_controller.rb
@@ -4,6 +4,14 @@ class MemoriesController < ApplicationController
   end
 
   def create
-    redirect_to :back
+    redirect_to :root
+    current_user.memories.create(memory_params)
+  end
+
+
+
+  private
+  def memory_params
+    params.require(:memory).permit(:text, :image, :user_friend, :no_user_friend)
   end
 end
