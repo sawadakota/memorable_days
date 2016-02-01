5ea4bdefaa5bb48e2751a62f853747df2ba7a196
澤田皇汰 <sawadakota@ksawada.local>
2016-02-02 01:20:12 +0900

    ・フォローのモデルを作った。

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160128075957) do
+ActiveRecord::Schema.define(version: 20160201104820) do
+
+  create_table "friendships", force: :cascade do |t|
+    t.integer  "following_id", limit: 4
+    t.integer  "follower_id",  limit: 4
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+  end
 
   create_table "memories", force: :cascade do |t|
     t.string   "text",           limit: 255
