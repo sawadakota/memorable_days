bbf46c6c06b6cc03350f1e6580b13dc28722125a
澤田皇汰 <sawadakota@ksawada.local>
2016-02-02 01:37:52 +0900

    ・改行した

--- db/migrate/20160201104820_create_friendships.rb
+++ db/migrate/20160201104820_create_friendships.rb
@@ -1,7 +1,6 @@
 class CreateFriendships < ActiveRecord::Migration
   def change
     create_table :friendships do |t|
-
       t.integer      :following_id
       t.integer      :follower_id
       t.timestamps null: false
