1858146ebbea5bbebf9187b425bf1d46a2e4d727
澤田皇汰 <sawadakota@ksawada.local>
2016-02-02 21:54:35 +0900

    ・フォロー、リムーブ機能実装！！

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -2,4 +2,9 @@ class FriendshipsController < ApplicationController
   def create
     @friendship = current_user.following.create(follower_id: params[:user_id])
   end
+
+  def destroy
+    @friendship = Friendship.find(params[:id])
+    @friendship.destroy
+  end
 end
