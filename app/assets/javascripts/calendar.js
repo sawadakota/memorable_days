606d4a66acf5cab16fed3de7eb2a1c816c1c1a66
澤田皇汰 <sawadakota@ksawada.local>
2016-02-14 14:01:06 +0900

    mortal tag user click change user page

--- app/assets/javascripts/calendar.js
+++ app/assets/javascripts/calendar.js
@@ -4,11 +4,12 @@ $(document).on('ready page:load', function(){
             var user_id = $('#page_set').val()
             var day = $(this).data("date")
             var guest = $('#guest_user').val()
+            var image = $("#" + day).val()
+            var num = 0
             // var top = $(this).offset().top
             // var left = $(this).offset().left
             // var top = top * 0.8
             // var left = left * 0.8
-            var image = $("#" + day).val()
             if (guest == "guest") {
               Object.keys(gon.memory_images).forEach(function (key) {
               if (key == day && guest == "guest") {
@@ -16,7 +17,8 @@ $(document).on('ready page:load', function(){
               // $('#comment_content').css({'left': left, 'top': top});
                   $('#comment_content').prepend('<img class="modal_image" style="background-image: url('+ gon.memory_images[key]+')" />')
                   $.each(gon.tag_image[day + "image"] , function(){
-                    $('#comment_image').append('<img class="mini_user_image" style="background-image: url('+ this +')" />')
+                    $('#comment_image').append('<a href= /users/'+ gon.tag_user_id[day + "id"][num] +' ><img class="mini_user_image" style="background-image: url('+ this +')" /></a>')
+                    num += 1
                   })
               }
             })
