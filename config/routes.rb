74816c03d1c9402edfe41c5fc945ed7358ca67fe
澤田皇汰 <sawadakota@ksawada.local>
2016-02-18 12:16:38 +0900

    user is able to share friend memory

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
     end
     member do
       get 'follows'
+      get 'friend'
     end
     resources :memories, only: [:new, :create, :edit, :update] do
       resources :comments, only: [:create]
