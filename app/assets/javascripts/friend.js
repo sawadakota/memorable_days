000e2175040aa92cbdd0c86b8bd08e145571bb67
澤田皇汰 <sawadakota@ksawada.local>
2016-02-18 18:42:47 +0900

    overtime function gon cansel

--- app/assets/javascripts/friend.js
+++ app/assets/javascripts/friend.js
@@ -1,13 +1,15 @@
 $(document).on('ready page:load', function(){
   var num = 0
-  while (num < gon.follows_count){
-    var top = Math.random() * (500 - 0)
-    var left = Math.random() * (900 - 0)
-    $('img', '#mini_user_image' + num).css("top", top);
-    $('img', '#mini_user_image' + num).css("left", left);
-    $('img', '#user_image' + num).css("top", top);
-    $("img", '#user_image' + num).css("left", left);
-    num += 1
-}
+  if num {
+    while (num < gon.follows_count){
+      var top = Math.random() * (500 - 0)
+      var left = Math.random() * (900 - 0)
+      $('img', '#mini_user_image' + num).css("top", top);
+      $('img', '#mini_user_image' + num).css("left", left);
+      $('img', '#user_image' + num).css("top", top);
+      $("img", '#user_image' + num).css("left", left);
+      num += 1
+  }
+  }
 }
 );
